8d9f01f2f5d381012298aa05e2a89900e725f8f9
Rohit Bokade <bokaderohit98@gmail.com>
2018-02-05 22:05:02 +0530

    Updated config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.generateAuthToken = function () {
 	var token = jwt.sign({
 		_id: user._id.toHexString(),
 		access
-	}, 'abc123');
+	}, process.env.JWT_SECRET);
 
 	user.tokens.push({
 		access,
@@ -74,7 +74,7 @@ UserSchema.statics.findByToken = function (token) {
 	var decoded = undefined;
 
 	try {
-		decoded = jwt.verify(token, 'abc123');
+		decoded = jwt.verify(token, process.env.JWT_SECRET);
 	} catch (err) {
 		return Promise.reject();
 	}
